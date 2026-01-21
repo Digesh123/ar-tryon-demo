e9a258ce32cf4061a026ae1dfb22dfe0d57018a3
Digesh <digesh.ghevade@courtsideanalytics.tech>
2026-01-22 03:10:02 +0530

    Initial ring ajaa yar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// import * as THREE from "three";
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // console.log("THREE OK:", THREE.REVISION);
 // console.log("MediaPipe Camera:", window.Camera);
